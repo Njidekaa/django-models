a7ca3bdebf8183148ca5e652fadef0f9736e8686
unknown <onyishiprecious73@gmail.com>
2022-06-18 19:52:56 +0100

    new additions

--- I4G0168521UX/blog/models.py
+++ I4G0168521UX/blog/models.py
@@ -1,3 +1,13 @@
+from tkinter import CASCADE
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title= models.CharField(max_length=200, unique=True)
+    text= models.TextField(max_length=250, unique=True)
+    author= models.ForeignKey(User, on_delete= models.CASCADE)
+    created_date= models.DateTimeField(default=timezone.now)
+    published_date= models.DateTimeField(default=timezone.now)
+
